be8ea6114b551a673766d88106a860ae36ba4b74
pdjpdjpdj <47047080+pdjpdjpdj@users.noreply.github.com>
2020-01-04 22:46:20 +0000

    Update vrtnu.get.js

--- vrtnu.get.js
+++ vrtnu.get.js
@@ -10,16 +10,21 @@ s_ajaxListener.callback = function () {
   // this.url    :the url of the requested script (including query string, if any) (urlencoded) 
   // this.data   :the data sent, if any ex: foo=bar&a=b (urlencoded)
   // print it in console
-  if(this.url.startsWith("https://media-services-public.vrt.be")){
+  //TODO: Debugging here
+  if(this.url.startsWith("https://media-services-public.vrt.be/vualto-video-aggregator-web")){
     console.log("Resending...")
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log(xhr.responseText);
-        }
-    }
-    xhr.open('GET', this.url, true);
-    xhr.send(null);
+    xhr.myXHR = true;
+    url = this.url;
+    method = "GET";
+    xhr.open(method, url, true);
+    xhr.onreadystatechange = function () {
+      console.log("YES"); // WHY U NO CALL?
+      if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        console.log(xhr.responseText);
+      }
+    };
+    xhr.send();
   }
   console.log(this.method);
   console.log(this.url);
@@ -28,22 +33,27 @@ s_ajaxListener.callback = function () {
 
 // Open for GET requests.
 XMLHttpRequest.prototype.open = function(a,b) {
-  if (!a) var a='';
-  if (!b) var b='';
-  s_ajaxListener.tempOpen.apply(this, arguments);
-  s_ajaxListener.method = a;  
-  s_ajaxListener.url = b;
-  if (a.toLowerCase() == 'get') {
-    s_ajaxListener.data = b.split('?');
-    s_ajaxListener.data = s_ajaxListener.data[1];
+  console.log(this)
+  if (!this.myXHR == true){
+    if (!a) var a='';
+    if (!b) var b='';
+    s_ajaxListener.tempOpen.apply(this, arguments);
+    s_ajaxListener.method = a;  
+    s_ajaxListener.url = b;
+    if (a.toLowerCase() == 'get') {
+      s_ajaxListener.data = b.split('?');
+      s_ajaxListener.data = s_ajaxListener.data[1];
+    }
   }
 }
 
 // Send for POST requests
 XMLHttpRequest.prototype.send = function(a,b) {
-  if (!a) var a='';
-  if (!b) var b='';
-  s_ajaxListener.tempSend.apply(this, arguments);
-  if(s_ajaxListener.method.toLowerCase() == 'post')s_ajaxListener.data = a;
-  s_ajaxListener.callback();
+  if (!this.myXHR == true){
+    if (!a) var a='';
+    if (!b) var b='';
+    s_ajaxListener.tempSend.apply(this, arguments);
+    if(s_ajaxListener.method.toLowerCase() == 'post')s_ajaxListener.data = a;
+    s_ajaxListener.callback();
+  }
 }
