201bd45aee3c742e73a8ba0baad955eb29d907c1
pdjpdjpdj <47047080+pdjpdjpdj@users.noreply.github.com>
2020-01-04 22:10:59 +0000

    Inception.

---
+++ vrtnu.get.js
@@ -0,0 +1,41 @@
+// From: http://stackoverflow.com/questions/3596583/javascript-detect-an-ajax-event
+// Kudos Crayon Violent
+// Small tweaks for this usecase.
+
+var s_ajaxListener = new Object();
+s_ajaxListener.tempOpen = XMLHttpRequest.prototype.open;
+s_ajaxListener.tempSend = XMLHttpRequest.prototype.send;
+s_ajaxListener.callback = function () {
+  // this.method :the ajax method used
+  // this.url    :the url of the requested script (including query string, if any) (urlencoded) 
+  // this.data   :the data sent, if any ex: foo=bar&a=b (urlencoded)
+  // print it in console
+  if(this.url.startsWith("https://media-services-public.vrt.be")){
+    console.log("yes")
+  }
+  console.log(this.method);
+  console.log(this.url);
+  console.log(this.data);
+}
+
+// Open for GET requests.
+XMLHttpRequest.prototype.open = function(a,b) {
+  if (!a) var a='';
+  if (!b) var b='';
+  s_ajaxListener.tempOpen.apply(this, arguments);
+  s_ajaxListener.method = a;  
+  s_ajaxListener.url = b;
+  if (a.toLowerCase() == 'get') {
+    s_ajaxListener.data = b.split('?');
+    s_ajaxListener.data = s_ajaxListener.data[1];
+  }
+}
+
+// Send for POST requests
+XMLHttpRequest.prototype.send = function(a,b) {
+  if (!a) var a='';
+  if (!b) var b='';
+  s_ajaxListener.tempSend.apply(this, arguments);
+  if(s_ajaxListener.method.toLowerCase() == 'post')s_ajaxListener.data = a;
+  s_ajaxListener.callback();
+}
